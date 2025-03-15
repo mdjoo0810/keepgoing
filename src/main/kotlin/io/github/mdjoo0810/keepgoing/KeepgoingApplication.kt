3c07b1814cfb9b849f2f118b6e4a972b6c58e1e3
mdjoo0810 <md.joo@rainist.com>
2025-03-15 16:50:22 +0900

    start

---
+++ src/main/kotlin/io/github/mdjoo0810/keepgoing/KeepgoingApplication.kt
@@ -0,0 +1,11 @@
+package io.github.mdjoo0810.keepgoing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KeepgoingApplication
+
+fun main(args: Array<String>) {
+    runApplication<KeepgoingApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/mdjoo0810/keepgoing/KeepgoingApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.mdjoo0810.keepgoing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KeepgoingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
