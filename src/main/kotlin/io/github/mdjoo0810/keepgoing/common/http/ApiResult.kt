0112877d0e24ea6ec7c7b45865d29b85392e2805
mdjoo0810 <md.joo@rainist.com>
2025-03-17 15:27:28 +0900

    feat: common api result

---
+++ src/main/kotlin/io/github/mdjoo0810/keepgoing/common/http/ApiResult.kt
@@ -0,0 +1,28 @@
+package io.github.mdjoo0810.keepgoing.common.http
+
+data class ApiResult<T> internal constructor(
+    val code: Int,
+    val message: String,
+    val result: T?,
+) {
+    companion object {
+        fun <T> success(result: T): ApiResult<T> {
+            return ApiResult(
+                code = 100,
+                message = "성공",
+                result = result,
+            )
+        }
+
+        fun <T> error(
+            code: Int,
+            message: String,
+        ): ApiResult<T> {
+            return ApiResult(
+                code = 100,
+                message = "성공",
+                result = null,
+            )
+        }
+    }
+}
